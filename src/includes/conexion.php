208929a5ba8598c25ac734fc646f1595820f2200
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-02-19 12:13:12 -0400

    Update conexion.php

--- src/includes/conexion.php
+++ src/includes/conexion.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$serverName = "GSISTEMA";
+$serverName = "172.16.1.39";
 $connectionInfo = array("Database" => "sis_control", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
-
+#
 
 ?>
