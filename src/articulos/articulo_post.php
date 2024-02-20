446c031a92fa2bdff8ac1eb75210f0e940a9b47c
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-02-20 15:46:55 -0400

    Update articulo_post.php

--- src/articulos/articulo_post.php
+++ src/articulos/articulo_post.php
@@ -71,7 +71,7 @@ if (isset($_FILES) && isset($_POST)) {
 
                     //ruta del destino del servidor
                     #$carpeta = $_SERVER['DOCUMENT_ROOT'] . '/php/PreviaShop_2.0/src/uploads/img/';
-                    #$carpeta =  "C:/inetpub/wwwroot/php/PreviaShop_2.0/src/uploads/img/";
+                    #$carpeta = $_SERVER['DOCUMENT_ROOT'] . '../uploads/img/';
                     //mover imagen a directorio temporal
                     #move_uploaded_file($_FILES['imagen']['tmp_name'], $carpeta . $art_img);
                     move_uploaded_file($_FILES['imagen']['tmp_name'], "../uploads/img/$art_img");
