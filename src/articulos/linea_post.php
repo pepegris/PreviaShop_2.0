4132c2e8c823624c886de1d514b76489b17ad384
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-02-20 11:03:56 -0400

    Update linea_post.php

--- src/articulos/linea_post.php
+++ src/articulos/linea_post.php
@@ -11,17 +11,17 @@ if (isset($_POST)) {
 
 
     
-    $linea_des=isset($_POST ['linea_des']) ? sqlsrv_escape_string($conn,$_POST['linea_des']):false ;
-    
+    /* $linea_des=isset($_POST ['linea_des']) ? sqlsrv_escape_string($conn,$_POST['linea_des']):false ; */
+    $linea_des=$_POST ['linea_des'] ;
 
     if ($conn) {
-        $sql= "INSERT INTO linea (linea_des,fecha) VALUES ('$linea_des',now())";
+        $sql= "INSERT INTO linea (linea_des,fecha) VALUES ('$linea_des',getdate())";
  
         $guardar = sqlsrv_query($conn,$sql);
 
         if (!$guardar) {
            
-            $error=sqlsrv_last_error($conn);
+            $error=sqlsrv_errors($conn);
            echo "<center><h3>ERROR</h3>";
            echo "<h4>$error</h4>";
 
