87cb7a528868c6843ae7799f5fc5e47da1ca47ac
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-02-20 15:19:46 -0400

    Update articulo_post.php

--- src/articulos/articulo_post.php
+++ src/articulos/articulo_post.php
@@ -36,7 +36,7 @@ if (isset($_FILES) && isset($_POST)) {
 
 
             //ruta del destino del servidor
-            $carpeta = $_SERVER['DOCUMENT_ROOT'] . '\php\PreviaShop_2.0\src\uploads\img';
+            $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/php/PreviaShop_2.0/src/uploads/img/';
 
             //almacenando nombre y direccion de la imagen
 
